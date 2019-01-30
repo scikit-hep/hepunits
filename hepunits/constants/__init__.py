cb8a8787f8196af1f31c82f1690d88ce8df4dd8d
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2019-01-30 21:54:49 +0100

    Add constants submodule from scikit-hep

---
+++ hepunits/constants/__init__.py
@@ -0,0 +1,29 @@
+# Licensed under a 3-clause BSD style license, see LICENSE.
+"""
+This module `hepunits.constants` contains 2 sorts of constants:
+
+  * Physical constants.
+  * Common and/or handy constants.
+
+All constants are computed in the HEP System of Units
+as defined in the `hepunits.units` package.
+
+Typical use case::
+
+    >>> from hepunits.constants import c_light
+    >>> from hepunits.units     import picosecond, micrometer
+    >>> tau_Bs = 1.5 * picosecond    # a particle lifetime, say the Bs meson's
+    >>> ctau_Bs = c_light * tau_Bs   # ctau of the particle, ~450 microns
+    >>> print ctau_Bs                # result in HEP units, so mm ;-)
+    0.449688687
+    >>> print ctau_Bs / micrometer   # result in micrometers
+    449.688687
+
+"""
+
+# -----------------------------------------------------------------------------
+# Import statements
+# -----------------------------------------------------------------------------
+from __future__ import absolute_import
+
+from .constants import *
