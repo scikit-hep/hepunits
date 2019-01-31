2fce515135adb5de2989d692f481b7ada4ec727b
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2019-01-31 10:37:04 +0100

    Finally added the setup.py file

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# Licensed under a 3-clause BSD style license, see LICENSE.
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os.path
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+def get_version():
+    g = {}
+    exec(open(os.path.join("hepunits", "version.py")).read(), g)
+    return g["__version__"]
+
+setup(
+    name = 'hepunits',
+    author = 'Eduardo Rodrigues',
+    author_email = 'eduardo.rodrigues@cern.ch',
+    maintainer = 'Eduardo Rodrigues',
+    maintainer_email = 'eduardo.rodrigues@cern.ch',
+    version = get_version(),
+    description = 'Units and constants in the HEP system of units',
+    long_description = open('README.rst').read(),
+    url = 'https://github.com/scikit-hep/hepunits',
+    license = 'new BSD',
+    packages = find_packages(),
+    include_package_data = True,
+    setup_requires = ['pytest-runner'],
+    tests_require = ['pytest'],
+    keywords = [
+        'HEP', 'units', 'constants',
+    ],
+    classifiers = [
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Development Status :: 4 - Beta',
+    ],
+    platforms = "Any",
+)
