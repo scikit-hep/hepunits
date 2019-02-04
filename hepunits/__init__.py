a85c770e7bab07db7813d0b1ab296ac359f4bad5
Henry Schreiner <henryschreineriii@utexas.edu>
2019-02-04 20:42:33 +0100

    Units directly accessible from the top level (#1)
    * Units directly accessible
    
    * Constants directly accessible

--- hepunits/__init__.py
+++ hepunits/__init__.py
@@ -4,3 +4,7 @@ from __future__ import absolute_import
 
 # Convenient access to the version number
 from .version import __version__
+
+# Units and constants directly available
+from .units import *
+from .constants import *
