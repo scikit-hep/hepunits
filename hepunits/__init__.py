26f3cec2212fa606acd0cb0abcb1fdb60e10269e
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2019-05-13 10:52:17 +0200

    Rename hepunits.version.py to hepunits._version.py, to hide the file (#5)

--- hepunits/__init__.py
+++ hepunits/__init__.py
@@ -1,9 +1,7 @@
 # Licensed under a 3-clause BSD style license, see LICENSE.
 
-from __future__ import absolute_import
-
 # Convenient access to the version number
-from .version import __version__
+from ._version import __version__
 
 # Units and constants directly available
 from .units import *
