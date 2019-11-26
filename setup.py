ea7a4c7b890f52a33917a82329c59651d802f76e
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2019-11-26 14:28:59 +0100

    Add Python 3.8 support and remove Python 3.4 support (#9)
    * Add Python 3.8 support and remove Python 3.4 support
    
    * Update azure-pipelines.yml accordingly
    
    * Update setup.py
    
    Co-Authored-By: Chris Burr <chrisburr@users.noreply.github.com>
    
    * While at it, add a MANIFEST.in
    
    * Include README as it's used by setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ import os
 from setuptools import setup
 from setuptools import find_packages
 
+
+PYTHON_REQUIRES = ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*"
+
 # Only add pytest-runner if the setup.py call needs it
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
@@ -29,9 +32,10 @@ setup(
     description = 'Units and constants in the HEP system of units',
     long_description = open('README.rst').read(),
     url = 'https://github.com/scikit-hep/hepunits',
-    license = 'new BSD',
+    license = 'BSD-3-Clause',
     packages = find_packages(),
     include_package_data = True,
+    python_requires = PYTHON_REQUIRES,
     setup_requires = [] + pytest_runner,
     tests_require = ['pytest'],
     keywords = [
@@ -48,10 +52,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Development Status :: 5 - Production/Stable',
     ],
     platforms = "Any",
