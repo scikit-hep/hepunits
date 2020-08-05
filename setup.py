135f1d5a2d61982a8b288b7251425255d85ff861
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2020-08-05 11:43:05 -0400

    fix: error instead of getting the wrong version
    These packages may be missing when running setup.py directly instead of through pip or pep517.build.

--- setup.py
+++ setup.py
@@ -3,4 +3,10 @@
 
 from setuptools import setup
 
+# Setup will not be able to find the version if these packages are missing
+# (Remember, setup.py does not run when installing from a wheel, so only required to make one)
+
+import setuptools_scm  # noqa: F401
+import toml  # noqa: F401
+
 setup()
