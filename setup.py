c19c841a7e419115f1bbe46badf59df28d23a9d6
Henry Schreiner <henryschreineriii@gmail.com>
2020-08-05 12:09:32 -0400

    style: fix style issues

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Licensed under a 3-clause BSD style license, see LICENSE.
 
 from setuptools import setup
 
 # Setup will not be able to find the version if these packages are missing
-# (Remember, setup.py does not run when installing from a wheel, so only required to make one)
+#
+# > Remember, setup.py does not run when installing from a wheel, so only
+# > required to make one
 
 import setuptools_scm  # noqa: F401
 import toml  # noqa: F401
