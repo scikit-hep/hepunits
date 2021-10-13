fafc6dbc18fea4d5bd5d697ec147ef897fabb338
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2021-10-13 10:30:44 +0200

    chore: various updates to config & CI files, re-add coverage tests (#47)
    * chore: miscellaneous updates to configuration files
    
    * chore: update .pre-commit-config.yaml
    
    * Re-add coverage tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Actually test Py 3.10
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -4,11 +4,4 @@
 
 from setuptools import setup
 
-# Setup will not be able to find the version if these packages are missing
-#
-# > Remember, setup.py does not run when installing from a wheel, so only
-# > required to make one
-
-import setuptools_scm  # noqa: F401
-
 setup()
